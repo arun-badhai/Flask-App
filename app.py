0dd70205dbebdd9b7d1e719557518d30eba285c4
srafay <rafay_syed1@yahoo.com>
2018-07-16 02:50:49 +0500

    Handling a POST request in Flask and building a mini-API

--- app.py
+++ app.py
@@ -5,5 +5,18 @@ app = Flask(__name__)
 def hello_world():
 	return "Hello World"
 
+@app.route('/add', methods=["POST"])
+def addNums():
+	dataDict = request.get_json()
+	if "x" not in dataDict or "y" not in dataDict:
+		return "Error, incomplete arguments!", 305
+	x = dataDict["x"]
+	y = dataDict["y"]
+	z = x+y
+	retJSON = {
+		"result": z
+	}
+	return jsonify(retJSON)
+
 if __name__ == "__main__":
 	app.run(debug=True)
