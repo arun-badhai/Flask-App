563cff8de4247bffc7895a1e4ad775ea37c168c0
srafay <rafay_syed1@yahoo.com>
2018-07-16 00:56:18 +0500

    Integrated different arithmatic operation resources into Arithmatic Resource to generalize the code

--- app.py
+++ app.py
@@ -5,44 +5,50 @@ app = Flask(__name__)
 api = Api(app)
 
 def checkPostedData(postedData, functionName):
-	if (functionName == "add"):
-			if "x" not in postedData or "y" not in postedData:
-				return 301
-				# 301 according to API Resource chart means that some params are missing
-			else:
-				return 200
+	if (functionName == "add" or functionName == "subtract" or functionName == "multiply" or functionName == "divide"):
+		if "x" not in postedData or "y" not in postedData:
+			return 301 # 301 according to API Resource chart means that some params are missing
+		if functionName == "divide" and postedData["y"] == 0:
+			return 302 # 302 according to API Resource chart means divide by zero error
+		else:
+			return 200
+	else:
+		return 601 # 601 according to API Resource chart means operation not supported
 
-class Add(Resource):
+def performArithmaticOperation(x,y, functionName):
+	if functionName == "add":
+		return x+y
+	if functionName == "subtract":
+		return x-y
+	if functionName == "multiply":
+		return x*y
+	if functionName == "divide":
+		return x/y
+
+
+class Arithmatic(Resource):
 	def post(self):
 		postedData = request.get_json()
-		statusCode = checkPostedData(postedData, "add")
+		try:
+			functionName = postedData["function"]
+		except:
+			return jsonify({"Message": "Error! no arithmatic operation specified"})
+		statusCode = checkPostedData(postedData, functionName)
 		if (statusCode != 200):
-			return ({
+			return jsonify({
 				"Message": "An error has occured!",	
 				"Status Code": statusCode
-			})
+				})
 		x= postedData["x"]
 		y= postedData["y"]
-		result = int(x) + int(y)
+		result = performArithmaticOperation(x,y, functionName)
 		retJSON = {
 			"Message": result,
 			"Status Code": 200
 		}
 		return jsonify(retJSON)
 
-class Subtract(Resource):
-	def post(self):
-		pass
-
-class Multiply(Resource):
-	def post(self):
-		pass
-
-class Divide(Resource):
-	def post(self):
-		pass
-
-api.add_resource(Add, "/add")
+api.add_resource(Arithmatic, "/arithmatic")
 
 
 @app.route('/')
